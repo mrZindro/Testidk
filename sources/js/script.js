f13b3cca0e3dce263eb46361849f6671aaac7de1
mrZindro <92520984+mrZindro@users.noreply.github.com>
2021-10-17 09:59:46 +0300

    Update script.js

---
+++ sources/js/script.js
@@ -0,0 +1,17 @@
+let i=0;
+console.log("цикл while");
+while (i<=5) {
+    console.log(i);
+    i++;
+}
+console.log("цикл do .... while");
+let b=0;
+do {
+    console.log(b);
+    b++;
+}   while (b<=5);
+let a=0;
+console.log('цикл for');
+for (;a<=5;a++) {
+    console.log(a);
+}
