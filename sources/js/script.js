73a6702486623c9d3258c7646023ab212c0b27ee
mrZindro <92520984+mrZindro@users.noreply.github.com>
2021-10-20 02:34:47 +0300

    dd

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,17 +1,25 @@
-let i=0;
-console.log("цикл while");
-while (i<=5) {
-    console.log(i);
-    i++;
-}
-console.log("цикл do .... while");
-let b=0;
-do {
-    console.log(b);
-    b++;
-}   while (b<=5);
-let a=0;
-console.log('цикл for');
-for (;a<=5;a++) {
-    console.log(a);
-}
+'use strict';
+
+// let i=0;
+// console.log("цикл while");
+// while (i<=5) {
+//     console.log(i);
+//     i++;
+// }
+// console.log("цикл do .... while");
+// let b=0;
+// do {
+//     console.log(b);
+//     b++;
+// }   while (b<=5);
+// let a=0;
+// console.log('цикл for');
+// for (;a<=5;a++) {
+//     console.log(a);
+// }
+// let g=1, h=2, j=3;
+// console.log(g, h, j);
+
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Родители разрешили?');
+// }
