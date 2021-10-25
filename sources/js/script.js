ee6f0101d4b967926d46482e52f879d2c2cbeb7b
mrzindro <mr.zindro@gmail.com>
2021-10-25 21:45:14 +0300

    justUp

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,25 +1,27 @@
 'use strict';
 
-// let i=0;
-// console.log("цикл while");
-// while (i<=5) {
-//     console.log(i);
-//     i++;
-// }
-// console.log("цикл do .... while");
-// let b=0;
-// do {
-//     console.log(b);
-//     b++;
-// }   while (b<=5);
-// let a=0;
-// console.log('цикл for');
-// for (;a<=5;a++) {
-//     console.log(a);
-// }
-// let g=1, h=2, j=3;
-// console.log(g, h, j);
+function stockList(listOfArt, listOfCat){
+    if (!listOfArt || !listOfCat) {return ''}
+    let count;
+    let counter;
+    let answer = [];
+    for (let i = 0; i < listOfCat.length; i++) {
+        count = listOfCat[i];
+        counter = 0;
+        for (let i = 0; i < listOfArt.length; i++) {
+            if (count == listOfArt[i][0]) {
+                counter += parseInt(listOfArt[i].replace(/[a-z]/gi, ''));
+            }
+            if (i == listOfArt.length-1) {
+                answer.push(`(${count} : ${counter})`)
+            }
+        }
+    }
+    return answer.join(' - ')
+}
+let b = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
+let c = ["A", "B", "C", "W"]
+console.log(stockList(b, c))
 
-// function checkAge(age) {
-//     return (age > 18) ? true : confirm('Родители разрешили?');
-// }
+
+
