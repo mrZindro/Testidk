9d8fddaed8283ca0df47051ef1bdff471e7b639e
mrzindro <mr.zindro@gmail.com>
2021-10-28 21:19:27 +0300

    yeahBoiii

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,27 +1,7 @@
 'use strict';
 
-function stockList(listOfArt, listOfCat){
-    if (!listOfArt || !listOfCat) {return ''}
-    let count;
-    let counter;
-    let answer = [];
-    for (let i = 0; i < listOfCat.length; i++) {
-        count = listOfCat[i];
-        counter = 0;
-        for (let i = 0; i < listOfArt.length; i++) {
-            if (count == listOfArt[i][0]) {
-                counter += parseInt(listOfArt[i].replace(/[a-z]/gi, ''));
-            }
-            if (i == listOfArt.length-1) {
-                answer.push(`(${count} : ${counter})`)
-            }
-        }
-    }
-    return answer.join(' - ')
+let bgMenu = document.querySelector('.burger')
+bgMenu.addEventListener('click', burgerMenu, true);
+function burgerMenu () {
+    document.querySelector('nav').classList.toggle('active');
 }
-let b = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
-let c = ["A", "B", "C", "W"]
-console.log(stockList(b, c))
-
-
-
