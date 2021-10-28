de41933e98ba3e2aa2764903bbb739f20862edde
mrzindro <mr.zindro@gmail.com>
2021-10-28 22:53:19 +0300

    someAnimations

--- sources/js/script.js
+++ sources/js/script.js
@@ -4,4 +4,6 @@ let bgMenu = document.querySelector('.burger')
 bgMenu.addEventListener('click', burgerMenu, true);
 function burgerMenu () {
     document.querySelector('nav').classList.toggle('active');
+    bgMenu.classList.toggle('active');
+
 }
