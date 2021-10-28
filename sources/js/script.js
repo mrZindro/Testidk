0f8eb01c939bb6662765fa8bb6189718d685b350
mrzindro <mr.zindro@gmail.com>
2021-10-29 00:47:08 +0300

    another

--- sources/js/script.js
+++ sources/js/script.js
@@ -5,5 +5,15 @@ bgMenu.addEventListener('click', burgerMenu, true);
 function burgerMenu () {
     document.querySelector('nav').classList.toggle('active');
     bgMenu.classList.toggle('active');
+}
 
+
+let spoiler = document.querySelector('main .container div');
+spoiler.addEventListener('click', spoilerToggle, false);
+function spoilerToggle(e) {
+    if (e.target != e.currentTarget) {
+        let clickedItem = e.target.id;
+        clickedItem('.container ul').classList.toggle('active');
+    }
+    e.stopPropagation();
 }
