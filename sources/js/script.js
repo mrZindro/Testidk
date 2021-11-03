466ed02ce4b2632698c185a88e90ff4ce0b42f2b
mrZindro <mr.zindro@gmail.com>
2021-11-03 13:41:32 +0300

    smallJSUP

--- sources/js/script.js
+++ sources/js/script.js
@@ -7,13 +7,36 @@ function burgerMenu () {
     bgMenu.classList.toggle('active');
 }
 
+let mainContainer = document.querySelector('main div');
+mainContainer.addEventListener('click', change, false);
 
-let spoiler = document.querySelector('main .container div');
-spoiler.addEventListener('click', spoilerToggle, false);
-function spoilerToggle(e) {
-    if (e.target != e.currentTarget) {
-        let clickedItem = e.target.id;
-        clickedItem('.container ul').classList.toggle('active');
+
+let listItems = document.querySelectorAll('main li');
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].className = `item${i}`
+    if (localStorage.getItem(`item${i}`)) {
+        listItems[i].textContent = localStorage.getItem(`item${i}`);
     }
+}
+function change(e) {
+    if (e.target != e.currentTarget) {
+        let clickedItem = e.target.tagName;
+        console.log('Hello ' + clickedItem);
+        if (e.target.tagName == 'LI') {
+            let newContent = prompt('Enter new content: ....');
+            if (newContent) {
+                localStorage.setItem(e.target.className, newContent);
+                }
+                let targetText = e.target.textContent;
+                e.target.textContent = newContent;
+            } else {
+                alert('You are wrong!')
+            }
+        }
+        // для слайдера
+        if (e.target.tagName == 'H2') {
+            e.target.parentNode.classList.toggle('active')
+        }
+    
     e.stopPropagation();
 }
